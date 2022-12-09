81d181d03adc00927dac37e3ccf11470e2ec80b4
art3mes <93867313+art3mes@users.noreply.github.com>
2022-12-09 22:23:23 +0530

    dropdown problem 40% solved

--- mal/script.js
+++ mal/script.js
@@ -1,4 +1,4 @@
-function right_dropdown_handler(){
+function right_dropdown_toggler(){
     var displayValue=document.querySelector(".cate").lastElementChild.style.display;
 
     if(displayValue==="none")
@@ -14,3 +14,39 @@ document.onclick = function(event) {
         document.querySelector(".cate").lastElementChild.style.display="none";
     }
   }
+
+//score dropdowns: the one with strings
+function score_string_dropdown_toggler(){
+    var displayValue=document.querySelector(".bluet1_drop").style.display;
+
+    if(displayValue==="none")
+    {
+        document.querySelector(".bluet1_drop").style.display="flex";
+    }
+    else{
+        document.querySelector(".bluet1_drop").style.display="none";
+    }
+}
+document.onclick = function(event) {
+    if (!event.target.matches('.bluet1_drop')) {
+        document.querySelector(".bluet1_drop").style.display="none";
+    }
+}
+
+//score dropdowns: the one with numbers
+function score_num_dropdown_toggler(){
+    var displayValue=document.querySelector(".bluet2_drop").style.display;
+
+    if(displayValue==="none")
+    {
+        document.querySelector(".bluet2_drop").style.display="flex";
+    }
+    else{
+        document.querySelector(".bluet2_drop").style.display="none";
+    }
+}
+document.onclick = function(event) {
+    if (!event.target.matches('.bluet2_drop')) {
+        document.querySelector(".bluet2_drop").style.display="none";
+    }
+}
