b20d299213df2d4491492e3bdef8dae8fd894807
art3mes <93867313+art3mes@users.noreply.github.com>
2022-12-10 23:04:38 +0530

    all dropdowns are working. Error: not closing when clicked again

--- mal/script.js
+++ mal/script.js
@@ -1,63 +1,17 @@
-/* function right_dropdown_toggler(){
-    var displayValue=document.querySelector(".cate").lastElementChild.style.display;
-
-    if(displayValue==="none")
-    {
-        document.querySelector(".cate").lastElementChild.style.display="flex";
-    }
-    else{
-        document.querySelector(".cate").lastElementChild.style.display="none";
-    }
-}
-
-
-//score dropdowns: the one with strings
-function score_string_dropdown_toggler(){
-    var displayValue=document.querySelector(".bluet1_drop").style.display;
-
-    if(displayValue==="none")
-    {
-        document.querySelector(".bluet1_drop").style.display="flex";
-    }
-    else{
-        document.querySelector(".bluet1_drop").style.display="none";
-    }
-}
-
-//score dropdowns: the one with numbers
-function score_num_dropdown_toggler(){
-    var displayValue=document.querySelector(".bluet2_drop").style.display;
-
-    if(displayValue==="none")
-    {
-        document.querySelector(".bluet2_drop").style.display="flex";
-    }
-    else{
-        document.querySelector(".bluet2_drop").style.display="none";
-    }
-}
-
-document.onclick = function(event) {
-    if (!event.target.matches(".cate, .bluet1_drop, .bluet2_drop")) {
-        document.querySelector(".cate").lastElementChild.style.display="none";
-        document.querySelector(".bluet1_drop").lastElementChild.style.display="none";
-        document.querySelector(".bluet2_drop").lastElementChild.style.display="none";
-    }
-  }
-//selection in dropdown */
 window.addEventListener('load', function() {
-    // wait until the page loads before working with HTML elements
     document.addEventListener('click', function(event) {
-      //click listener on the document
-      document.querySelectorAll('.dropdown').forEach(function(el) {
-        if (el !== event.target) 
-            el.classList.remove('show');
-        // close any showing dropdown that isn't the one just clicked
+      document.querySelectorAll('.dropdown,.bluet1_drop,.bluet2_drop').forEach(function(class_holder) {
+        if (class_holder !== event.target) 
+            class_holder.classList.remove('show');
       });
       if (event.target.matches('.cate')) {
-        event.target.closest('.cate').querySelector('.dropdown').classList.toggle('show')
+        event.target.closest('.cate').querySelector('.dropdown').classList.toggle('show');
       }
-      // if this is a dropdown button being clicked, toggle the show class
+      if (event.target.matches('.bluet1')) {
+        event.target.closest('.bluet1').querySelector('.bluet1_drop').classList.toggle('show');
+      }
+      if (event.target.matches('.bluet2')) {
+        event.target.closest('.bluet2').querySelector('.bluet2_drop').classList.toggle('show');
+      }
     })
-  })
-  
+}) 
