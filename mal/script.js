6ff547d80babf4c1b39490825fc92f517e245082
art3mes <93867313+art3mes@users.noreply.github.com>
2022-12-10 22:51:37 +0530

    one dropdown working

--- mal/script.js
+++ mal/script.js
@@ -1,4 +1,4 @@
-function right_dropdown_toggler(){
+/* function right_dropdown_toggler(){
     var displayValue=document.querySelector(".cate").lastElementChild.style.display;
 
     if(displayValue==="none")
@@ -9,11 +9,7 @@ function right_dropdown_toggler(){
         document.querySelector(".cate").lastElementChild.style.display="none";
     }
 }
-document.onclick = function(event) {
-    if (!event.target.matches('.cate')) {
-        document.querySelector(".cate").lastElementChild.style.display="none";
-    }
-  }
+
 
 //score dropdowns: the one with strings
 function score_string_dropdown_toggler(){
@@ -24,11 +20,6 @@ function score_string_dropdown_toggler(){
         document.querySelector(".bluet1_drop").style.display="flex";
     }
     else{
-        document.querySelector(".bluet1_drop").style.display="none";
-    }
-}
-document.onclick = function(event) {
-    if (!event.target.matches('.bluet1_drop')) {
         document.querySelector(".bluet1_drop").style.display="none";
     }
 }
@@ -45,10 +36,28 @@ function score_num_dropdown_toggler(){
         document.querySelector(".bluet2_drop").style.display="none";
     }
 }
-document.onclick = function(event) {
-    if (!event.target.matches('.bluet2_drop')) {
-        document.querySelector(".bluet2_drop").style.display="none";
-    }
-}
 
-//selection in dropdown
+document.onclick = function(event) {
+    if (!event.target.matches(".cate, .bluet1_drop, .bluet2_drop")) {
+        document.querySelector(".cate").lastElementChild.style.display="none";
+        document.querySelector(".bluet1_drop").lastElementChild.style.display="none";
+        document.querySelector(".bluet2_drop").lastElementChild.style.display="none";
+    }
+  }
+//selection in dropdown */
+window.addEventListener('load', function() {
+    // wait until the page loads before working with HTML elements
+    document.addEventListener('click', function(event) {
+      //click listener on the document
+      document.querySelectorAll('.dropdown').forEach(function(el) {
+        if (el !== event.target) 
+            el.classList.remove('show');
+        // close any showing dropdown that isn't the one just clicked
+      });
+      if (event.target.matches('.cate')) {
+        event.target.closest('.cate').querySelector('.dropdown').classList.toggle('show')
+      }
+      // if this is a dropdown button being clicked, toggle the show class
+    })
+  })
+  
