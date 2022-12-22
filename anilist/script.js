f596a171c8a48af9f212907415e191857470da1b
art3mes <93867313+art3mes@users.noreply.github.com>
2022-12-22 17:26:07 +0530

    js implemented for the spoile tags

--- anilist/script.js
+++ anilist/script.js
@@ -1,4 +1,17 @@
 function spoiler_toggler(){
-    var display=document.getElementsByClassName("spoiler-body")[0];
+    var display=document.getElementsByClassName("spoiler-body")[0].style.display;
+    var numOfElement=document.getElementsByClassName("spoiler-body").length;
+    
+    for (let index = 0; index < numOfElement; index++) {
+
+        if (display === "none") {
+            document.getElementsByClassName("spoiler-body")[index].style.display="flex";
+        }
+        
+        else{
+            document.getElementsByClassName("spoiler-body")[index].style.display="none";  
+        }
+    }
+    
+
 }
-//implement js
