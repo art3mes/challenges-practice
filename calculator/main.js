d98102ccdc0c5df81f249e26362a2f3f95bbd6d6
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-03 13:44:55 +0530

    operatorS

--- calculator/main.js
+++ calculator/main.js
@@ -2,3 +2,7 @@ function number(num){
     var innerValue=num.innerHTML;
     console.log(innerValue);
 }
+function operation(sym){
+    var symbolValue=sym.innerHTML;
+    console.log(symbolValue);
+}
