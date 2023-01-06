43c9341893cfa6368e1fcda74270bb24248e839f
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-06 21:14:02 +0530

    :

--- calculator/main.js
+++ calculator/main.js
@@ -1,19 +1,27 @@
 function number(num){
-    var innerValue=num.innerHTML;
+    var innerValue=num.innerText;
     console.log(innerValue);
     display(innerValue);
 }
 function operation(ope){
-    var operator=ope.innerHTML;
+    var operator=ope.innerText;
     console.log(operator);
 }
 function specialOperation(sOpe){
-    var specialOperator=sOpe.innerHTML;
+    var specialOperator=sOpe.innerText;
     console.log(specialOperator);
 }
 function editButtons(edit){
-    var button=edit.innerHTML;
+    var button=edit.innerText;
     console.log(button);
+    if(button==="CE" || button==="C"){
+        document.querySelector(".calc-display").value="";
+    }
+    if(button==="DEL"){
+        var lenghtOfValue=document.querySelector(".calc-display").value.length;
+        document.querySelector(".calc-display").value=document.querySelector(".calc-display").value.substring(0,lenghtOfValue-1);
+        console.log(lenghtOfValue);
+    }
 }
 
 function display(value){
