95bd719fa1bdc3732c01db24fc74ca1fadce9b4d
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-07 16:49:41 +0530

    eval method implemented

--- calculator/main.js
+++ calculator/main.js
@@ -5,7 +5,7 @@ function number(num){
 }
 function operation(ope){
     var operator=ope.innerText;
-    
+    document.querySelector(".calc-display").value+=operator;
     console.log(operator);
     
 }
@@ -23,6 +23,9 @@ function editButtons(edit){
         var lenghtOfValue=document.querySelector(".calc-display").value.length;
         document.querySelector(".calc-display").value=document.querySelector(".calc-display").value.substring(0,lenghtOfValue-1);
         console.log(lenghtOfValue);
+    }
+    if(button==="="){
+        document.querySelector(".calc-display").value=eval(document.querySelector(".calc-display").value);
     }
 }
 function display(value){
