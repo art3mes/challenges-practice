267def44e04d175e1b50e1cb7e6192612e65b954
art3mes <93867313+art3mes@users.noreply.github.com>
2023-01-09 19:34:51 +0530

    display function implemented for centralised display

--- calculator/main.js
+++ calculator/main.js
@@ -8,11 +8,13 @@ function number(num){
 function operation(ope){
     var operator=ope.innerText;
     if(c===0){
-        document.querySelector(".calc-display").value+=operator;
+        //document.querySelector(".calc-display").value+=operator;
+        outputCalc(document.querySelector(".calc-display").value+operator);
     }
     else{
         c=0;
-        document.querySelector(".calc-display").value+=operator;
+        //document.querySelector(".calc-display").value+=operator;
+        outputCalc(document.querySelector(".calc-display").value+operator);
     }   
     console.log(operator);   
 }
@@ -21,40 +23,52 @@ function specialOperation(sOpe){
     console.log(specialOperator);
     if(specialOperator==="1/x"){
         c=1;
-        document.querySelector(".calc-display").value=1/eval(document.querySelector(".calc-display").value);
+        //document.querySelector(".calc-display").value=1/eval(document.querySelector(".calc-display").value);
+        outputCalc(1/eval(document.querySelector(".calc-display").value));
     }
     if(specialOperator==="x2"){
         c=1;
-        document.querySelector(".calc-display").value=Math.pow(eval(document.querySelector(".calc-display").value),2);
+        //document.querySelector(".calc-display").value=Math.pow(eval(document.querySelector(".calc-display").value),2);
+        outputCalc(Math.pow(eval(document.querySelector(".calc-display").value),2));
     }
     if(specialOperator==="x1/2"){
         c=1;
-        document.querySelector(".calc-display").value=Math.pow(eval(document.querySelector(".calc-display").value),1/2);
+        //document.querySelector(".calc-display").value=Math.pow(eval(document.querySelector(".calc-display").value),1/2);
+        outputCalc(Math.pow(eval(document.querySelector(".calc-display").value),1/2));
     }
 }
 function editButtons(edit){
     var button=edit.innerText;
     console.log(button);
     if(button==="CE" || button==="C"){
-        document.querySelector(".calc-display").value="";
+        //document.querySelector(".calc-display").value="";
+        outputCalc("");
     }
     if(button==="DEL"){
         var lenghtOfValue=document.querySelector(".calc-display").value.length;
-        document.querySelector(".calc-display").value=document.querySelector(".calc-display").value.substring(0,lenghtOfValue-1);
+        //document.querySelector(".calc-display").value=document.querySelector(".calc-display").value.substring(0,lenghtOfValue-1);
+        outputCalc(document.querySelector(".calc-display").value.substring(0,lenghtOfValue-1));
         console.log(lenghtOfValue);
     }
     if(button==="="){
         c=1;                            
-        document.querySelector(".calc-display").value=eval(document.querySelector(".calc-display").value);
+        //document.querySelector(".calc-display").value=eval(document.querySelector(".calc-display").value);
+        outputCalc(eval(document.querySelector(".calc-display").value));
     }
 }
 function display(value){
     if(c===0){
-        document.querySelector(".calc-display").value+=value;
+        //document.querySelector(".calc-display").value+=value;
+        outputCalc(document.querySelector(".calc-display").value+value);
     }
     else{
         c=0;
-        document.querySelector(".calc-display").value="";
-        document.querySelector(".calc-display").value+=value;
+        //document.querySelector(".calc-display").value="";
+        outputCalc("");
+        //document.querySelector(".calc-display").value+=value;
+        outputCalc(document.querySelector(".calc-display").value+value);
     }
+}
+function outputCalc(value){
+    document.querySelector(".calc-display").value=value;
 }
