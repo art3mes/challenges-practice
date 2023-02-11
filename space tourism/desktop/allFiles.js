a099efc60437794998f166ecc4c2cd948eb31617
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-11 17:23:53 +0530

    hmm

--- space tourism/desktop/allFiles.js
+++ space tourism/desktop/allFiles.js
@@ -1,5 +1,24 @@
-function changer(){
-    //window.open("http://www.google.com","_self");
-    console.log("in");
-    window.open("destination-moon.html","_self");
-}
+$(document).ready(function(){
+    $(".header-right div").on("click",function(event){
+        var target = event.target || event.srcElement;
+        console.log(target.textContent); 
+        target=target.textContent;
+
+        if(target=="00"|| target=="00 HOME"){
+            window.open("index.html","_self");
+            console.log(target);
+        }
+        if(target=="01"|| target=="01 DESTINATION"){
+            window.open("destination-moon.html","_self");
+            console.log(target); 
+        }
+        if(target=="02" || target== "02 CREW"){
+            window.open("crew-commander.html","_self");
+            console.log(target); 
+        }
+        if(target=="03" || target == "03 TECHNOLOGY"){
+            window.open("technology-vehicle.html","_self");
+            console.log(target); 
+        }
+    });
+});
