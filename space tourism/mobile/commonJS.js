89b57b7c6843d725f2d29c9fbe354a2c8a7fd30a
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-17 22:04:12 +0530

    e

--- space tourism/mobile/commonJS.js
+++ space tourism/mobile/commonJS.js
@@ -1,98 +1,99 @@
-
-$(document).ready(function(){
-  var flag=0;    //zero means sidebar is closed
-  function sidebar(){
-    if(flag===0){
-      document.querySelector(".header-right").style.opacity=0;
-      document.querySelector(".sidebar").style.display="flex";
-    flag=1;
-    }
-    else{
-      document.querySelector(".header-right").style.opacity=1;
-      document.querySelector(".sidebar").style.display="none";
-      flag=0;
-    }
+console.log("lmaop");
+var flag=0;    //zero means sidebar is closed
+function sidebar(){
+  if(flag===0){
+    document.querySelector(".header-right").style.opacity=0;
+    document.querySelector(".sidebar").style.display="flex";
+  flag=1;
   }
+  else{
+    document.querySelector(".header-right").style.opacity=1;
+    document.querySelector(".sidebar").style.display="none";
+    flag=0;
+  }
+}
 
-  $(".header-right div").on("click",function(event){
-      var target = event.target || event.srcElement;
-                                                              //do it using the word searching "textContent" if the error persists
-      target=target.getAttribute("data-id");
+// $(document).ready(function(){
+  
+// //   $(".header-right div").on("click",function(event){
+// //       var target = event.target || event.srcElement;
+// //                                                               //do it using the word searching "textContent" if the error persists
+// //       target=target.getAttribute("data-id");
 
-      if(target==0){
-          window.open("index.html","_self");
+// //       if(target==0){
+// //           window.open("index.html","_self");
           
-      }
-      if(target==1){
-          window.open("destination-moon.html","_self");
+// //       }
+// //       if(target==1){
+// //           window.open("destination-moon.html","_self");
          
-      }
-      if(target==2){
-          window.open("crew-commander.html","_self");
+// //       }
+// //       if(target==2){
+// //           window.open("crew-commander.html","_self");
          
-      }
-      if(target==3){
-          window.open("technology-vehicle.html","_self");
+// //       }
+// //       if(target==3){
+// //           window.open("technology-vehicle.html","_self");
          
-      }
-  });
+// //       }
+// //   });
 
-  $(".body-left-scroll div").on("click",function(event){
-      var target=event.target || event.srcElement;
-      target=target.getAttribute("data-id");
+// //   $(".body-left-scroll div").on("click",function(event){
+// //       var target=event.target || event.srcElement;
+// //       target=target.getAttribute("data-id");
      
-      if(target==0){
-          window.open("crew-commander.html","_self");
-      }
-      if(target==1){
-          window.open("crew-specialist.html","_self");
-      }
-      if(target==2){
-          window.open("crew-pilot.html","_self");
-      }
-      if(target==3){
-          window.open("crew-engineer.html","_self");
-      }
-  });
-  $(".body-right-top div").on("click",function(event){
-      var target=event.target || event.srcElement;
-      target=target.getAttribute("data-id");
-      if(target==0){
-          window.open("destination-moon.html","_self");
-      }
-      if(target==1){
-          window.open("destination-mars.html","_self");
-      }
-      if(target==2){
-          window.open("destination-europa.html","_self");
-      }
-      if(target==3){
-          window.open("destination-titan.html","_self");
-      }
+// //       if(target==0){
+// //           window.open("crew-commander.html","_self");
+// //       }
+// //       if(target==1){
+// //           window.open("crew-specialist.html","_self");
+// //       }
+// //       if(target==2){
+// //           window.open("crew-pilot.html","_self");
+// //       }
+// //       if(target==3){
+// //           window.open("crew-engineer.html","_self");
+// //       }
+// //   });
+// //   $(".body-right-top div").on("click",function(event){
+// //       var target=event.target || event.srcElement;
+// //       target=target.getAttribute("data-id");
+// //       if(target==0){
+// //           window.open("destination-moon.html","_self");
+// //       }
+// //       if(target==1){
+// //           window.open("destination-mars.html","_self");
+// //       }
+// //       if(target==2){
+// //           window.open("destination-europa.html","_self");
+// //       }
+// //       if(target==3){
+// //           window.open("destination-titan.html","_self");
+// //       }
 
-  });
-  $(".body-left-scroll div").on("click",function(event){
-      var target=event.target|| event.srcElement;
-      // target=target.getAttribute("data-id");
-      target=target.innerHTML;
-      console.log(target);
-      // if(target==="vehicle"){
-      //     window.open("technology-vehicle.html","_self");
-      // }
-      // if(target==="spaceport"){
-      //     window.open("technology-spaceport.html","_self");
-      // }
-      // if(target==="capsule"){
-      //     window.open("technology-capsule.html","_self");
-      // }
-      if(target==="<span>1</span>" || target==="1"){
-          window.open("technology-vehicle.html","_self");
-      }
-      if(target==="<span>2</span>" || target==="2"){
-          window.open("technology-spaceport.html","_self");
-      }
-      if(target==="<span>3</span>" ||target==="3"){
-          window.open("technology-capsule.html","_self");
-      }
-  });
-});
+// //   });
+// //   $(".body-left-scroll div").on("click",function(event){
+// //       var target=event.target|| event.srcElement;
+// //       // target=target.getAttribute("data-id");
+// //       target=target.innerHTML;
+// //       console.log(target);
+// //       // if(target==="vehicle"){
+// //       //     window.open("technology-vehicle.html","_self");
+// //       // }
+// //       // if(target==="spaceport"){
+// //       //     window.open("technology-spaceport.html","_self");
+// //       // }
+// //       // if(target==="capsule"){
+// //       //     window.open("technology-capsule.html","_self");
+// //       // }
+// //       if(target==="<span>1</span>" || target==="1"){
+// //           window.open("technology-vehicle.html","_self");
+// //       }
+// //       if(target==="<span>2</span>" || target==="2"){
+// //           window.open("technology-spaceport.html","_self");
+// //       }
+// //       if(target==="<span>3</span>" ||target==="3"){
+// //           window.open("technology-capsule.html","_self");
+// //       }
+// //   });
+// });
