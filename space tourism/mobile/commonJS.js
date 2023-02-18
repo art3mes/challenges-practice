66e6902730689f05d381585e1ebea02a385eed8f
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-18 12:05:46 +0530

    crew working

--- space tourism/mobile/commonJS.js
+++ space tourism/mobile/commonJS.js
@@ -37,23 +37,23 @@ $(document).ready(function(){
       }
   });
 
-//   $(".body-left-scroll div").on("click",function(event){
-//       var target=event.target || event.srcElement;
-//       target=target.getAttribute("data-id");
+  $(".body-buttons div").on("click",function(event){
+      var target=event.target || event.srcElement;
+      target=target.getAttribute("data-id");
      
-//       if(target==0){
-//           window.open("crew-commander.html","_self");
-//       }
-//       if(target==1){
-//           window.open("crew-specialist.html","_self");
-//       }
-//       if(target==2){
-//           window.open("crew-pilot.html","_self");
-//       }
-//       if(target==3){
-//           window.open("crew-engineer.html","_self");
-//       }
-//   });
+      if(target==0){
+          window.open("crew-commander.html","_self");
+      }
+      if(target==1){
+          window.open("crew-specialist.html","_self");
+      }
+      if(target==2){
+          window.open("crew-pilot.html","_self");
+      }
+      if(target==3){
+          window.open("crew-engineer.html","_self");
+      }
+  });
 //   $(".body-right-top div").on("click",function(event){
 //       var target=event.target || event.srcElement;
 //       target=target.getAttribute("data-id");
