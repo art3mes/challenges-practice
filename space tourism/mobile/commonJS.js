30f5339fca10b58fa5e060630929521805c9794a
art3mes <93867313+art3mes@users.noreply.github.com>
2023-02-18 12:14:47 +0530

    site working. mext task: simplification and bundelling

--- space tourism/mobile/commonJS.js
+++ space tourism/mobile/commonJS.js
@@ -71,28 +71,28 @@ $(document).ready(function(){
       }
 
    });
-//   $(".body-left-scroll div").on("click",function(event){
-//       var target=event.target|| event.srcElement;
-//       // target=target.getAttribute("data-id");
-//       target=target.innerHTML;
-//       console.log(target);
-//       // if(target==="vehicle"){
-//       //     window.open("technology-vehicle.html","_self");
-//       // }
-//       // if(target==="spaceport"){
-//       //     window.open("technology-spaceport.html","_self");
-//       // }
-//       // if(target==="capsule"){
-//       //     window.open("technology-capsule.html","_self");
-//       // }
-//       if(target==="<span>1</span>" || target==="1"){
-//           window.open("technology-vehicle.html","_self");
-//       }
-//       if(target==="<span>2</span>" || target==="2"){
-//           window.open("technology-spaceport.html","_self");
-//       }
-//       if(target==="<span>3</span>" ||target==="3"){
-//           window.open("technology-capsule.html","_self");
-//       }
-//   });
+  $(".body-buttons div").on("click",function(event){
+      var target=event.target|| event.srcElement;
+      // target=target.getAttribute("data-id");
+      target=target.innerHTML;
+      console.log(target);
+      // if(target==="vehicle"){
+      //     window.open("technology-vehicle.html","_self");
+      // }
+      // if(target==="spaceport"){
+      //     window.open("technology-spaceport.html","_self");
+      // }
+      // if(target==="capsule"){
+      //     window.open("technology-capsule.html","_self");
+      // }
+      if(target==="<span>1</span>" || target==="1"){
+          window.open("technology-vehicle.html","_self");
+      }
+      if(target==="<span>2</span>" || target==="2"){
+          window.open("technology-spaceport.html","_self");
+      }
+      if(target==="<span>3</span>" ||target==="3"){
+          window.open("technology-capsule.html","_self");
+      }
+  });
 });
